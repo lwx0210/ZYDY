d0181565b673446b51e63d39f4a983d6d738fdc2
Lee Moon hee <ways0210@qq.com>
2025-04-29 23:39:46 +0800

    Add files via upload

--- AwemeHeaders.h
+++ AwemeHeaders.h
@@ -49,13 +49,6 @@ typedef NS_ENUM(NSInteger, MediaType) {
 @interface AWESearchAwemeExtraModel : NSObject
 @end
 
-@interface AWEModernLongPressInteractiveCell : UITableViewCell
-@property (nonatomic, strong) UICollectionView *collectionView;
-@property (nonatomic, strong) AWELongPressPanelViewGroupModel *longPressViewGroupModel;
-@property (nonatomic, strong) NSArray *dataArray;
-@property (nonatomic, assign) BOOL isAppearing;
-@end
-
 @interface AWEAwemeTextExtraModel : NSObject
 @property (nonatomic, copy) NSString *hashtagName;
 @property (nonatomic, copy) NSString *hashtagId;
@@ -124,12 +117,16 @@ typedef NS_ENUM(NSInteger, MediaType) {
 @property (nonatomic) BOOL hasMore;
 @property (nonatomic) BOOL isModern;
 @property (nonatomic) BOOL isDYYYCustomGroup;
+- (void)setIsDYYYCustomGroup:(BOOL)isCustom;
 @end
 
 @interface AWEModernLongPressHorizontalSettingCell : UITableViewCell
 @property (nonatomic, strong) UICollectionView *collectionView;
 @property (nonatomic, strong) NSArray *dataArray;
 @property (nonatomic, strong) AWELongPressPanelViewGroupModel *longPressViewGroupModel;
+
+- (void)setupCustomLayout;
+- (CGFloat)widthForText:(NSString *)text;
 @end
 
 @interface AWEModernLongPressHorizontalSettingItemCell : UICollectionViewCell
