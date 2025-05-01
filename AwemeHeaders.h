78b7eff3334ede1ef9e1b8147e03df21ba3623a8
Lee Moon hee <ways0210@qq.com>
2025-05-02 01:23:31 +0800

    Update AwemeHeaders.h

--- AwemeHeaders.h
+++ AwemeHeaders.h
@@ -897,6 +897,9 @@ typedef NS_ENUM(NSInteger, MediaType) {
 - (void)setShouldShowToggle:(BOOL)arg1;
 - (NSUInteger)animationStyle;
 - (NSUInteger)viewStyle;
+- (void)setSlideDismissBlock:(void (^)(void))slideDismissBlock;
+- (void)setTapDismissBlock:(void (^)(void))tapDismissBlock;
+- (void)setAfterDismissBlock:(void (^)(void))afterDismissBlock;
 @end
 
 @interface AWEIMEmoticonModel : NSObject
