9e144a31b3c23fa76756a6e24fa067fc4b72f1f4
Lee Moon hee <ways0210@qq.com>
2025-05-05 22:56:24 +0800

    Update DYYYConstants.h

--- DYYYConstants.h
+++ DYYYConstants.h
@@ -6,7 +6,7 @@
 #define DYYY_SETTINGS_NAME @"DYYY设置"
 
 // 版本信息
-#define DYYY_VERSION @"2.2-5"
-#define DYYY_VERSION_STRING @"v2.2-5"
+#define DYYY_VERSION @"2.2-6"
+#define DYYY_VERSION_STRING @"v2.2-6"
 
 #endif
