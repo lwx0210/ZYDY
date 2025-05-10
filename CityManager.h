af5467b9f052a881559b406b8c62e93fcd7875a4
Lee Moon hee <ways0210@qq.com>
2025-05-10 08:51:57 +0800

    Update CityManager.h

--- CityManager.h
+++ CityManager.h
@@ -3,15 +3,14 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface CityManager : NSObject
-@property (nonatomic, strong) NSDictionary *cityCodeMap;
+
 
 + (instancetype)sharedInstance;
 
 - (nullable NSString *)getProvinceNameWithCode:(NSString *)code;
 - (nullable NSString *)getCityNameWithCode:(NSString *)code;
 - (nullable NSString *)getDistrictNameWithCode:(NSString *)code;
-- (nullable NSString *)getStreetNameWithCode:(NSString *)code;
-- (void)loadCityData;
+- (nullable NSString *)getStreetNameWithCode:(NSString *)
 + (void)fetchLocationWithGeonameId:(NSString *)geonameId completionHandler:(void (^)(NSDictionary *locationInfo, NSError *error))completionHandler;
 @end
 
